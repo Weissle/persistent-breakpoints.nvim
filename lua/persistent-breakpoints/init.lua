9f5717c3fbe7f425811248792c9da0505a390d35
weissle <2016301500298@whu.edu.cn>
2022-07-20 22:20:37 +0800

    fix bug: error occurs when user's config is an empty table.

--- lua/persistent-breakpoints/init.lua
+++ lua/persistent-breakpoints/init.lua
@@ -4,11 +4,11 @@ local cfg = require('persistent-breakpoints.config')
 local M = {}
 
 M.setup = function(_cfg)
+	_cfg = _cfg or {}
 	local tmp_config = vim.tbl_deep_extend('force',cfg,_cfg)
 	for key,val in pairs(tmp_config) do
 		cfg[key] = val
 	end
-	vim.fn.extend(cfg,_cfg or {},'force')
 	inmem_bps.bps = utils.load_bps(utils.get_bps_path()) -- {'filename':breakpoints_table}
 	utils.create_path(cfg.save_dir)
 end
